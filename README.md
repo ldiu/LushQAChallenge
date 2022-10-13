2d6c0cd918318397e5b512b060aac53c9c1485f7
Liana Diu <liana.diu@hotmail.com>
2022-10-13 07:16:24 -0700

    Added Readme Contents and scripting to add second item to cart.

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Lush QA Developer Co-op Technical Challenge
 
 ### Introduction
+This project was created for the Lush QA Developer Co-op technical Challenge. It is a script that functions to navigate to https://www.lush.ca, add two items to the cart, navigate to the cart and take a screenshot showing the results of the automation. 
+
+### How to run:
+1. Download git files
+2. Install npm/node
+3. Install puppeteer
+4. Run 'node index.js'
+
+### Credits
+[Puppeteer Documentation](https://pptr.dev/)
+[Solution for Modals](https://stackoverflow.com/questions/55258562/puppeteer-close-javascript-alert-box)
+[For timer promise](https://pptr.dev/api/puppeteer.page.waitfortimeout/) [+](https://stackoverflow.com/questions/46919013/puppeteer-wait-n-seconds-before-continuing-to-the-next-line)
+
+
+Written by Liana Diu on October 13th, 2022
--- index.js
+++ index.js
@@ -5,15 +5,12 @@ const puppeteer = require('puppeteer');
     const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
 
-
     const locationPopUpCloseSelector = "#header > div > div.utility-nav.bg-jet > div > wainclude > div > div.dropdown-menu.mt-0 > div > div > div > div:nth-child(2) > button";
     const menuHoverSelector = "#mainNavbar > div > ul > li:nth-child(4) > button";
     const categorySelector = "#bath-bombs";
     const bathBombPopUpCloseSelector = "#email-signup-modal > div > div > div > div.subscription-modal-header.border-0.mx-4.pull-right > button";
-   
-    // #product-search-results > div > div.col-sm-12.col-md-8.col-lg-9 > div.row.product-grid > div:nth-child(2) > div > div > div.product-tile-body.d-flex.flex-column.position-relative.w-100.h-100.text-center > div.product-tile-buy-row > div.cart-and-ipay > button
-    // #product-search-results > div > div.col-sm-12.col-md-8.col-lg-9 > div.row.product-grid > div:nth-child(1) > div > div > div.product-tile-body.d-flex.flex-column.position-relative.w-100.h-100.text-center > div.product-tile-buy-row > div.cart-and-ipay > button
-    // #product-search-results > div > div.col-sm-12.col-md-8.col-lg-9 > div.row.product-grid > div:nth-child(3) > div > div > div.product-tile-body.d-flex.flex-column.position-relative.w-100.h-100.text-center > div.product-tile-buy-row > div.cart-and-ipay > button
+    const bathBomb1 = "#product-search-results > div > div.col-sm-12.col-md-8.col-lg-9 > div.row.product-grid > div:nth-child(1) > div > div > div.product-tile-body.d-flex.flex-column.position-relative.w-100.h-100.text-center > div.product-tile-buy-row > div.cart-and-ipay > button";
+    const bathBomb2 = "#product-search-results > div > div.col-sm-12.col-md-8.col-lg-9 > div.row.product-grid > div:nth-child(2) > div > div > div.product-tile-body.d-flex.flex-column.position-relative.w-100.h-100.text-center > div.product-tile-buy-row > div.cart-and-ipay > button";
     
     await page.setViewport({
         width: 1920,
@@ -35,10 +32,6 @@ const puppeteer = require('puppeteer');
         console.log("Unable to close location pop-up window");
     }
 
-
-
-
-
     await page.hover(menuHoverSelector);
 
     try {
@@ -53,21 +46,45 @@ const puppeteer = require('puppeteer');
     try {
         await page.waitForSelector(bathBombPopUpCloseSelector);
         await page.click((bathBombPopUpCloseSelector));
+        await page.waitForNavigation();
     } catch (error) {
-        console.log("Unable to close pop-up in bath bomb window (or there is no pop-up)");
+        console.log("Unable to close pop-up in bath bomb window");
     }
-
-    // const bathBombs = await page.$$('.add-to-cart-btn');
-
 
     try {
-        new Promise(r => setTimeout(r, 3000));
-        await page.waitForSelector("#product-search-results > div > div.col-sm-12.col-md-8.col-lg-9 > div.row.product-grid > div:nth-child(2) > div > div > div.product-tile-body.d-flex.flex-column.position-relative.w-100.h-100.text-center > div.product-tile-buy-row > div.cart-and-ipay > button");
-        await page.click(("#product-search-results > div > div.col-sm-12.col-md-8.col-lg-9 > div.row.product-grid > div:nth-child(2) > div > div > div.product-tile-body.d-flex.flex-column.position-relative.w-100.h-100.text-center > div.product-tile-buy-row > div.cart-and-ipay > button"));
+        await page.waitForSelector(bathBomb1);
+        await page.click((bathBomb1));
+        await page.waitForNavigation();
     } catch (error) {
-        console.log("Unable to close pop-up in bath bomb window (or there is no pop-up)");
-    }
+        console.log("Unable to add first item");
+    };
+
+    try {
+        await page.waitForSelector("#add-to-cart-modal > div > div > div.d-flex > button");
+        await page.click("#add-to-cart-modal > div > div > div.d-flex > button");
+        await page.waitForNavigation();
+    } catch (error) {
+        console.log("Unable to close mini window");
+    };
+  
+    try {
+
+        await page.waitForSelector(bathBomb2);
+        await page.click((bathBomb2));
+        await page.waitForNavigation();
+    } catch (error) {
+        console.log("Unable to add second item");
+    };
+
+    try {
+
+        await page.waitForSelector( "#add-to-cart-modal > div > div > div.minicart-footer.mt-2.mx-3.mx-md-0 > div:nth-child(3) > div > div > button");
+        await page.click( "  #add-to-cart-modal > div > div > div.minicart-footer.mt-2.mx-3.mx-md-0 > div:nth-child(3) > div > div > button");
+        await page.waitForNavigation();
+    } catch (error) {
+        console.log("Unable to go to cart");
+    };
 
     await page.screenshot({ path: "TestResults/AutomationResult.png" });
-    // await browser.close();
+    await browser.close();
 })();
